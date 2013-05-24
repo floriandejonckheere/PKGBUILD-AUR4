58676eecaf4d3ef6d566168409640f39f51b8a46
Florian Dejonckheere <florian@floriandejonckheere.be>
2013-05-24 12:22:26 +0200

    Cleaned up cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -8,11 +8,23 @@
 # List of suffixes to search for. Suffixes with a trailing slash will be considered paths.
 SUFFIX=('*.tar.gz' '*.tar.bz2' '*.zip' 'src/' 'pkg/')
 
+CLEAN=()
+
 for SUF in ${SUFFIX[@]}; do
-	METHOD="name"
-	[[ $(echo ${SUF} | tail -c 2) == "/" ]] && METHOD="path"
-	echo "Searching for $SUF, with method $METHOD"
-	for TRASH in $(find . -i${METHOD} ${SUF}); do
-		echo $TRASH
+	TYPE="f"
+	[[ "${SUF}" == */ ]] && TYPE="d" && SUF=$(echo ${SUF} | head -c -2)
+	echo "Searching for ${SUF}, type ${TYPE}"
+	for TRASH in $(find . -type ${TYPE} -iname ${SUF}); do
+		echo -e "=> \033[1;31m$TRASH\033[0m"
+		CLEAN=(${CLEAN[@]} $TRASH)
 	done
 done
+
+echo -n "Continue? THIS WILL RECURSIVELY DELETE (y/N) "
+read -n 1 CONT
+echo
+if [[ "$CONT" == "y" ]]; then
+	for TRASH in "${CLEAN[@]}"; do
+		rm -ir "${TRASH}"
+	done
+fi
