77b6d10506f4b26c8fadd8cc9748b8cf373e10d9
Florian Dejonckheere <florian@floriandejonckheere.be>
2015-02-05 10:04:19 +0100

    Updated prepare-src

--- prepare-src.sh
+++ prepare-src.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/bash
 #
-# prepare-src.sh - downloads and prepares AUR packages
+# prepare-src.sh - downloads and extracts AUR packages
 #
 # Florian Dejonckheere <florian@floriandejonckheere.be>
 #
 
-[[ ! $1 ]] && echo "Usage: $0 PACKAGE [EXTRACT_DIR]" && exit 1
+if [[ ! $1 ]]; then
+	echo -e "\e[1;32mUsage: $0 PACKAGE ...\e[0m" 1>&2
+	exit 1
+fi
 
-[[ $1 ]] && PKG=$(echo $1 | tr [:upper:] [:lower:])
+for ARG in $@; do
+	PKG=$(echo ${ARG} | tr [:upper:] [:lower:])
 
-DIR="$(pwd)/$2"
+	echo -en "\e[1;33mProcessing package ${PKG}... "
+	wget --quiet "https://aur.archlinux.org/packages/$(echo ${PKG} | cut -b -2)/${PKG}/${PKG}.tar.gz" -O "/tmp/${PKG}.tar.gz" || 
+	{
+		echo -e "\e[0m\n\e[1;32mError: package ${PKG} not found.\e[0m"
+		exit 1
+	}
+	echo -e "Done\e[0m"
 
-wget "https://aur.archlinux.org/packages/$(echo ${PKG} | cut -b -2)/${PKG}/${PKG}.tar.gz" -O "/tmp/${PKG}.tar.gz"
-[[ ! -d "${DIR}" ]] && mkdir "${DIR}"
-tar zxvf "/tmp/${PKG}.tar.gz" -C "${DIR}"
+	[[ ! -d "${PKG}" ]] && mkdir "${PKG}"
+	echo
+	tar zxvf "/tmp/${PKG}.tar.gz" -C "$(pwd)"
+done
